95f1953418cb8be9c7ef758d1865281e1d421e99
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-10 23:09:03 -0500

    feat: create pagination React context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import MainNav from "@/components/MainNav";
+import { PaginationContextProvider } from "@/components/PaginationContext";
+import PokemonTable from "@/components/PokemonTable";
 
 export default function Home() {
   return (
     <main className="flex container min-h-screen flex-col items-center pt-2 divide-y-2">
       <MainNav />
+      <PaginationContextProvider>
+        <PokemonTable />
+      </PaginationContextProvider>
     </main>
   );
 }
