f1904a3e67253fbc62f131872b721841e9215a54
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-11 14:51:45 -0500

    feat: helper function for extract search params

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function extractOffSetAndLimit(url: string): {
+  offsetNumber: number | null;
+  limitNumber: number | null;
+} {
+  const urlObj = new URL(url);
+  const params = new URLSearchParams(urlObj.search);
+
+  const offsetNumber = params.has("offset")
+    ? Number(params.get("offset"))
+    : null;
+  const limitNumber = params.has("limit") ? Number(params.get("limit")) : null;
+
+  return { offsetNumber, limitNumber };
 }
