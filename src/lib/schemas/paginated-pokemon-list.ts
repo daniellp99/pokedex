43ccb70b1d7d998c64adffdd471b0e58911d453f
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-11 08:30:25 -0500

    fix: next url string in pagination can be null

--- src/lib/schemas/paginated-pokemon-list.ts
+++ src/lib/schemas/paginated-pokemon-list.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const paginatedPokemonListSchema = z.object({
   count: z.number(),
-  next: z.string(),
+  next: z.string().nullable(),
   previous: z.string().nullable(),
   results: z.array(z.object({ name: z.string(), url: z.string() })),
 });
