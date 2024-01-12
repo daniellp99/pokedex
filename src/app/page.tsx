ac85d7cb0c450faa32d4e62656c2c903c445665e
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-11 21:23:11 -0500

    feat: render pokemon in a table with basic info
    In mobile view will render a list with the same info
    
    Fixes #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import PokemonTable from "@/components/PokemonTable";
 
 export default function Home() {
   return (
-    <main className="flex container min-h-screen flex-col items-center pt-2 divide-y-2">
+    <main className="flex container min-h-screen flex-col items-center pt-2">
       <MainNav />
       <PaginationContextProvider>
-        <PokemonTable />
+        <section className="size-full">
+          <PokemonTable />
+        </section>
       </PaginationContextProvider>
     </main>
   );
