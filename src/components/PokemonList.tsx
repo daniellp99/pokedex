3377b54e1c422f98e30ba3a9e557e1c711d25f86
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-12 14:51:14 -0500

    feat: List of cards for render pokemons in small devices
    Fixes #9

---
+++ src/components/PokemonList.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { LoaderIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { usePaginatedPokemonList } from "@/hooks/usePaginatedPokemonList";
+import { usePokemon } from "@/hooks/usePokemon";
+import { usePaginationContext } from "./PaginationContext";
+import PokemonStatBadges from "./PokemonStatBadges";
+import PokemonAvatar from "./PokemonAvatar";
+
+function PokemonCard({ name }: { name: string }) {
+  const { pokemon, isError, isLoading } = usePokemon(name);
+
+  if (isLoading) return <p>Loading</p>;
+
+  // TODO make a fallback component in case of error
+  if (isError || !pokemon) return null;
+  return (
+    <Card className="bg-accent">
+      <CardHeader className="pt-2 pb-0">
+        <CardTitle className="capitalize text-center">
+          {pokemon.name.replace("-", " ")}
+        </CardTitle>
+      </CardHeader>
+      <CardContent className="grid grid-cols-3 gap-3 place-items-center pb-0">
+        <ul className="col-span-2">
+          <li>
+            <p className="capitalize ">
+              Ability: {pokemon.moves[0].move.name.replace("-", " ")}
+            </p>
+          </li>
+          <li>
+            <p className="capitalize">
+              Move: {pokemon.abilities[0].ability.name.replace("-", " ")}
+            </p>
+          </li>
+        </ul>
+        <PokemonAvatar
+          avatar={{ name: pokemon.name, sprites: pokemon.sprites }}
+        />
+      </CardContent>
+      <CardFooter className="grid grid-cols-3 gap-1 w-fit pb-2">
+        <PokemonStatBadges stats={pokemon.stats} />
+      </CardFooter>
+    </Card>
+  );
+}
+
+function CardList({ offset, limit }: { offset: number; limit: number }) {
+  const { list, isError, isLoading } = usePaginatedPokemonList(offset, limit);
+
+  if (isLoading)
+    return (
+      <li className="place-self-center">
+        <LoaderIcon className="flex sm:hidden animate-spin size-10" />
+      </li>
+    );
+  // TODO make a fallback component in case of error
+  if (isError || !list) return null;
+
+  return (
+    <>
+      {list.results.map((pokemon) => (
+        <li key={pokemon.name}>
+          <PokemonCard name={pokemon.name} />
+        </li>
+      ))}
+    </>
+  );
+}
+
+export default function PokemonList() {
+  const { offset, setOffset, limit, setLimit } = usePaginationContext();
+
+  const pages = [];
+  for (let i = 0; i <= offset / limit; i++) {
+    pages.push(
+      <CardList
+        offset={i * limit}
+        limit={limit}
+        key={`${i * limit}-${limit}`}
+      />
+    );
+  }
+
+  const handlePageChange = () => {
+    setOffset(offset + limit);
+    setLimit(limit);
+  };
+
+  return (
+    <ul className="flex flex-col gap-2 sm:hidden">
+      {pages}
+      <li className="place-self-center">
+        <Button
+          onClick={() => {
+            handlePageChange();
+          }}
+        >
+          Load More
+        </Button>
+      </li>
+    </ul>
+  );
+}
