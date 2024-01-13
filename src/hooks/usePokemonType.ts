3f136056bf72a4508997ad5228486abc95131d31
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-13 18:41:49 -0500

    feat: fetcher for get all pokemon  types

--- src/hooks/usePokemonType.ts
+++ src/hooks/usePokemonType.ts
@@ -2,8 +2,9 @@ import useSWR from "swr";
 
 import fetcher from "@/lib/fetcher";
 import { PokemonTypeExtended } from "@/lib/schemas/pokemon-type";
+import { paginatedPokemonList } from "@/lib/schemas/paginated-pokemon-list";
 
-export function usePokemonType(name = "") {
+export function usePokemonType(name: string) {
   const { data, error, isLoading } = useSWR(
     `https://pokeapi.co/api/v2/type/${name}`,
     fetcher<PokemonTypeExtended>
@@ -11,6 +12,18 @@ export function usePokemonType(name = "") {
 
   return {
     pokemonType: data,
+    isLoading,
+    isError: error,
+  };
+}
+export function useAllTypes() {
+  const { data, error, isLoading } = useSWR(
+    "https://pokeapi.co/api/v2/type/",
+    fetcher<paginatedPokemonList>
+  );
+
+  return {
+    allTypes: data,
     isLoading,
     isError: error,
   };
