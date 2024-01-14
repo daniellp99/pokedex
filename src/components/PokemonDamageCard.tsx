0d4d20157d5bd61b6a92106979f128a7526f641a
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-13 19:23:10 -0500

    feat: damage relations card component

---
+++ src/components/PokemonDamageCard.tsx
@@ -0,0 +1,77 @@
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { useAllTypes, usePokemonType } from "@/hooks/usePokemonType";
+import { Pokemon, PokemonType } from "@/lib/schemas/pokemon-detail";
+import PokemonTypeBadge from "./PokemonTypeBadge";
+import { cn } from "@/lib/utils";
+
+function PokemonDamageCardItem({
+  iterType,
+  typeName,
+}: {
+  iterType: string;
+  typeName: string;
+}) {
+  const { pokemonType, isError, isLoading } = usePokemonType(typeName);
+
+  if (isLoading) return null;
+
+  // TODO make a fallback component in case of error
+  if (isError || !pokemonType) return null;
+
+  return (
+    <div className="flex flex-col items-center ">
+      <p
+        className={cn(
+          "text-muted-foreground text-sm invisible",
+          pokemonType.damage_relations.double_damage_from.find(
+            (item) => item.name === iterType
+          ) && "visible",
+          pokemonType.damage_relations.half_damage_from.find(
+            (item) => item.name === iterType
+          ) && "visible"
+        )}
+      >
+        {pokemonType.damage_relations.double_damage_from.find(
+          (item) => item.name === iterType
+        )
+          ? 1
+          : pokemonType.damage_relations.half_damage_from.find(
+              (item) => item.name === iterType
+            )
+          ? 1 / 2
+          : 0}
+      </p>
+      <PokemonTypeBadge key={iterType} name={iterType} />
+    </div>
+  );
+}
+
+export default function PokemonDamageCard({ type }: { type: PokemonType }) {
+  const { allTypes, isError, isLoading } = useAllTypes();
+
+  if (isLoading) return null;
+
+  // TODO make a fallback component in case of error
+  if (isError || !allTypes) return null;
+
+  return (
+    <Card className="border-none">
+      <CardTitle className="">
+        <p className="font-medium text-muted-foreground text-center text-lg">
+          Damage Relations
+        </p>
+      </CardTitle>
+      <CardContent className="grid grid-cols-4 gap-2">
+        {allTypes.results.map((item) => (
+          <PokemonDamageCardItem
+            key={item.name}
+            iterType={item.name}
+            typeName={type.type.name}
+          />
+        ))}
+      </CardContent>
+    </Card>
+  );
+}
