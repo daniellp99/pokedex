baf00b72f7a7f48def0a64e3cd4a79476c5427cb
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-14 08:35:30 -0500

    Improve swr performance Fixes #15

--- src/components/PokemonProvider.tsx
+++ src/components/PokemonProvider.tsx
@@ -3,9 +3,30 @@ import { PaginationContextProvider } from "@/components/PaginationContext";
 import PokemonList from "@/components/PokemonList";
 import PokemonTable from "@/components/PokemonTable";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { SWRConfig } from "swr";
 
 export default function PokemonProvider() {
   const isDesktop = useMediaQuery("(min-width: 640px)");
-  if (isDesktop) return <PokemonTable />;
-  return <PokemonList />;
+
+  if (isDesktop)
+    return (
+      <SWRConfig
+        value={{
+          refreshInterval: 10 * 24 * 60 * 60 * 1000, //10 días
+          revalidateOnFocus: false,
+        }}
+      >
+        <PokemonTable />
+      </SWRConfig>
+    );
+  return (
+    <SWRConfig
+      value={{
+        refreshInterval: 10 * 24 * 60 * 60 * 1000, //10 días
+        revalidateOnFocus: false,
+      }}
+    >
+      <PokemonList />
+    </SWRConfig>
+  );
 }
